d7cba186d6a0fe0e5ca1144ce2ca858b6265e399
crypt0nking <75346247+crypt0nking@users.noreply.github.com>
2026-09-04 22:55:24 +0200

    Update upstream-proxy.ts

--- lib/upstream-proxy.ts
+++ lib/upstream-proxy.ts
@@ -1,5 +1,5 @@
 const WORDPRESS_ORIGIN =
-  process.env.WORDPRESS_ORIGIN || 'https://kisiselgelisimforum.com'
+  process.env.WORDPRESS_ORIGIN || 'https://orveb.com'
 
 const PASSTHROUGH_HEADERS = [
   'content-type',
